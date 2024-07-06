993b28d66b7e40148aa0a3acc52fb4e50f1ef8d7
irashovdra <109846925+irashovdra@users.noreply.github.com>
2024-07-06 14:01:49 -0500

    finished color-picker

--- src/js/color-picker.js
+++ src/js/color-picker.js
@@ -3,6 +3,20 @@ import colorsTemplate from "../templates/colors.hbs";
 import colors from "../data/colors.json";
 const body = document.querySelector("body");
 
+const colorBox = document.querySelector(".colors__box");
+
 const colorsList = colorsTemplate({ colors });
 
+const text = document.querySelector(".text");
+
 body.insertAdjacentHTML("beforeend", colorsList);
+
+const colorsPallete = document.querySelector(".colors");
+
+colorsPallete.addEventListener("click", selectColor);
+
+function selectColor(event) {
+  if (event.target.nodeName === "DIV") {
+    text.style.color = event.target.style.backgroundColor;
+  }
+}
